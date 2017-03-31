8fc890848699ae593ba75a82da1678f590d457ed
Corentin <corentin.lelez@gmail.com>
2017-03-31 21:05:12 +0200

    null

--- player.cpp
+++ player.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "player.h"
 
+player::player(int No, SDL_renderer *renderer,std::string imgname)
+{
+    SDL_surface* tmp_surface = SDL_LoadBMP(imgname.c_str());
+    if (tmp_surface==null) std::cout <<"Image non chargée " <<SDL_GetError() << std::endl;
+
+    m_texture=SDL_CreateTexture(renderer, tmp_surface);
+    if (m_surface==null) std::cout <<"Texture non chargée " <<SDL_GetError() << std::endl;
+
+
+
+
+
+}
--- player.h
+++ player.h
@@ -1,1 +1,18 @@
 
+#include <SDL.h>
+
+class player
+{
+public:
+
+    player(int No, SDL_renderer *renderer, std::string imgname);
+    virtual ~image();
+
+    int height;
+    int width;
+
+
+private:
+    SDL_Texture* m_texture;
+    SDL_Rect m_pos;
+};
